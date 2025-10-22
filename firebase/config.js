e7d32eca9974a02b65cd434b358aa68dabfc4714
aleksandar-stotka <aangelevski6@gmail.com>
2025-10-22 11:03:33 +0200

    first commit

---
+++ firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD2rMCn6-nYjXzwzZJUxDhq0ThjpYBc3vo",
+  authDomain: "macedon-ani.firebaseapp.com",
+  projectId: "macedon-ani",
+  storageBucket: "macedon-ani.firebasestorage.app",
+  messagingSenderId: "578129065475",
+  appId: "1:578129065475:web:16b555775457e8f07ec036"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="p-4">
+    <h1 class="text-xl font-bold">Counter: {{ counter.count }}</h1>
+    <p>Double: {{ counter.doubleCount }}</p>
+
+    <div class="mt-4 space-x-2">
+      <button @click="counter.increment" class="px-3 py-1 bg-green-600 text-white rounded">+</button>
+      <button @click="counter.decrement" class="px-3 py-1 bg-red-600 text-white rounded">-</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useCategory } from '@/stores/useCategory'
+const counter = useCategory()
+</script>
